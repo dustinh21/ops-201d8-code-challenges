94329975d89dca2bb000d925f0c9b8335429695a
lisbon <dustin@iroquois.capital>
2023-04-27 03:21:04 +0000

    completed challenge 3

--- printhistory.sh
+++ printhistory.sh
@@ -6,15 +6,20 @@
 # Author:                       Dustin H
 # Date of latest revision:      00/00/0000
 # Purpose:                      Purpose
-var="hello world"
 
 # Declaration of variables
 
 # Declaration of functions
+print_login_history () {
+    last
+    echo "This is the login history"
+}
 
 
 # Main
-
+print_login_history
+print_login_history
+print_login_history
 
 
 # End
