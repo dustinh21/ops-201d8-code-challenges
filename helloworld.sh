47d8f8e74f7c2683878635e94b92a0f3c67716b2
lisbon <dustin@iroquois.capital>
2023-04-27 03:04:47 +0000

    updated README.md and helloworld

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+# Main
+
+#!/bin/bash
+
+# Script Name:                  Script Name
+# Author:                       Dustin H
+# Date of latest revision:      00/00/0000
+# Purpose:                      Purpose
+var="hello world"
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+
+
+
+# End
